bf9b51e706ccdaf60f8779e7f0bc74c50e3fc46a
PauloBritez <90757552+PauloBritez@users.noreply.github.com>
2022-02-09 05:29:27 -0300

    all

---
+++ metodo-de-String/script.js
@@ -0,0 +1,21 @@
+let nome = "Juan Paulo Britez Braga"; // string
+
+//console.log(nome.length); // retorna o tamanho da string
+
+//let resultado = nome.indexOf("Britez"); // retorna o indice do primeiro caracter da string
+
+/*let resultado = '';
+
+if (nome.indexOf("antonio") != -1) {
+    resultado = "Encontrei";
+} else {
+    resultado = "Não encontrei";
+} // retorna o indice do primeiro caracter da string */
+
+//let resultado = nome.slice(0, -5); // retorna a string de 0 a -5
+
+//let resultado = nome.substring(8, 13); // retorna a string de 8 a 13
+
+let resultado = nome.substr(8, 13); // retorna a string de 8 a 13
+
+console.log(resultado); // retorna o tamanho da string
