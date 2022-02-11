a1eedd3c51af18fd0bb5aa6f82d2e18deced1f3f
PauloBritez <90757552+PauloBritez@users.noreply.github.com>
2022-02-11 19:28:26 -0300

    new

--- metodo-de-String/script.js
+++ metodo-de-String/script.js
@@ -1,4 +1,4 @@
-let nome = "Juan Paulo Britez Braga"; // string
+let nome = "juan paulo"; // string
 
 //console.log(nome.length); // retorna o tamanho da string
 
@@ -16,6 +16,55 @@ if (nome.indexOf("antonio") != -1) {
 
 //let resultado = nome.substring(8, 13); // retorna a string de 8 a 13
 
-let resultado = nome.substr(8, 13); // retorna a string de 8 a 13
+/*let resultado = nome.substr(8, 13); // retorna a string de 8 a 13
 
-console.log(resultado); // retorna o tamanho da string
+console.log(resultado); // retorna o tamanho da string*/
+
+//let resultado = nome.replace("carros", "moto"); // substitui a string
+
+//let resultado = nome.toUpperCase(); // retorna a string em maiusculo
+
+//let resultado = nome.toLowerCase(); // retorna a string em minusculo
+
+//let resultado = nome.concat(" e motos"); // concatena a string
+
+//let resultado = nome + " e motos"; // concatena a string
+
+//let resultado = nome.trim(); // retorna a string sem espaços em branco
+
+//let resultado = nome.charAt(1); // retorna o caracter da posição 1
+
+//let resultado = nome[0]; // retorna o caracter da posição 1
+
+//let resultado = nome.split(" "); // retorna um array com os elementos da string
+
+console.log(resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ metodos-de-Numbers/script.js
@@ -0,0 +1,10 @@
+let n = 10.7587547;
+
+//let res = n.toString(); //convertir a string
+
+//let res = n + 5; //soma
+
+//let res = n.toString() + 5; //concatenação
+
+let res = n.toFixed(2); //retorna o número com 2 casas decimais
+console.log(res);
