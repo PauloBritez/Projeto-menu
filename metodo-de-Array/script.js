df3ecb511e1875cbd851525ed346f11c35499d4e
PauloBritez <90757552+PauloBritez@users.noreply.github.com>
2022-02-14 19:53:05 -0300

    new

--- metodo-de-Array/script.js
+++ metodo-de-Array/script.js
@@ -1,5 +1,29 @@
 let lista = ['Ovo','Farinha','Manteiga','Acucar'];
 
-let res = lista.toString(); 
+//let res = lista.toString(); // transforma o array em uma string
+
+//let res = lista.join('-'); // transforma o array em uma string
+
+//let res = lista.indexOf('Ovos'); // retorna o indice do elemento
+
+//lista.shift(); // remove o primeiro elemento do array
+
+//lista.pop(); // remove o ultimo elemento do array
+
+//lista.push('Leite'); // adiciona um elemento no final do array
+
+//lista.unshift('Leite'); // adiciona um elemento no inicio do array
+
+//lista.splice(2,0,'Chocolate'); // adiciona um elemento no indice 2 do array
+
+//lista[0]= 'Leite'; // substitui o elemento do indice 0 pelo elemento 'Leite'
+
+//lista.reverse(); // inverte o array
+
+//lista.sort(); // ordena o array
+
+lista[lista.length] = 'Chocolate'; // adiciona um elemento no final do array
+
+let res = lista;
 
 console.log(res);
