4bf731758959d3154033e4c5ae650ba507edcd81
PauloBritez <90757552+PauloBritez@users.noreply.github.com>
2022-02-16 19:47:15 -0300

    otro new

--- Datas/script.js
+++ Datas/script.js
@@ -1,13 +1,52 @@
-let d = new Date( '2019-02-16 19:06:56' );
-//let dia = d.getDate();
-//let mes = d.getMonth() + 1;
-//let ano = d.getFullYear();
-
-//console.log(d.toDateString()); //${dia}/${mes}${ano}
-
-console.log(d.toUTCString());
 
 
+//console.log(d.toDateString()); // pega a data atual no formato DD/MM/YYYY
+/*let d = new Date( '2021, 9' ); // parâmetros: ano, mês 
+
+console.log(d); // mostra a data no formato ISO */
+
+//let d = new Date(); //pega a data atual do sistema operacional do usuario 
+
+//let novoValor = d.getDay(); // pega o dia da semana da data atual
+//let novoValor = d.getDate(); // pega o dia do mês da data atual
+//let novoValor = d.getMonth(); // pega o mês da data atual
+//let novoValor = d.getFullYear(); // pega o ano atual da data atual
+//let novoValor = d.getHours(); //pega a hora atual 0-23 
+//let novoValor = d.getMinutes(); // pega os minutos do dia atual 
+//let novoValor = d.getSeconds(); // pega o segundo atual do relógio do computador 
+//let novoValor = d.getMilliseconds(); // pegando milisegundos 0-999 
+//let novoValor = d.getTime(); // pega o timestamp do dia em milisegundos (1 dia = 86400000 milisegundos) 
+//let novoValor = d.getTimezoneOffset(); // pega o fuso horario do usuario no momento da criacao da data (UTC)
+//let novoValor = Date.now(); //timestamp atual em milisegundos 
+
+
+let d = new Date(); //pega a data atual do sistema operacional do usuario   //console.log(d);
+
+//d.setFullYear(2028); // troca o ano
+//d.setMonth(1); // troca o mês
+//d.setFullYear(d.getFullYear() + 1); // acrescenta 1 ano
+//d.setMonth(1); // troca o mês
+//d.setMonth(1, 1); // troca o mês e o dia
+//d.setMonth(d.getMonth() + 1); // acrescenta 1 mês
+//d.setDate(1); // troca o dia
+//d.setDate(d.getDate() + 1); // adiciona um dia
+//d.setHours(0); // troca a hora
+//d.setHours(d.getHours() + 1); // acrescenta 1 hora
+//d.setHours(0, 0); // troca a hora e os minutos
+//d.setMinutes(0); // troca os minutos
+//d.setMinutes(d.getMinutes() + 1); // acrescenta 1 minuto
+//d.setMinutes(0, 0); // troca os minutos e os segundos
+//d.setSeconds(0); // troca os segundos
+//d.setSeconds(d.getSeconds() + 1); // acrescenta 1 segundo
+//d.setSeconds(0, 0); // troca os segundos e os milisegundos
+//d.setMilliseconds(0); // troca os milisegundos
+//d.setMilliseconds(d.getMilliseconds() + 1); // acrescenta 1 milisegundo
+//d.setMilliseconds(0, 0); // troca os milisegundos e os microsegundos
+
+
+let novoValor = d; // troca o valor da variavel d
+
+console.log(novoValor); //troca o valor da variavel d
 
 
 
