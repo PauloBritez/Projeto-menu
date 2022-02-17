658be6213f0b8578397970f80473c7de0c123dfe
PauloBritez <90757552+PauloBritez@users.noreply.github.com>
2022-02-17 18:39:54 -0300

    nuevo

---
+++ operacoes-matematica/script.js
@@ -0,0 +1,24 @@
+//let novoValor = Math.floor(Math.random() * 100) + 1; // Gera um número aleatório entre 1 e 100
+
+//let novoValor = Math.PI; // pi = 3.141592653589793 (número pi) 
+//let novoValor = Math.E; // e = 2.718281828459045 (número e)
+//let novoValor = Math.SQRT2; // sqrt(2) = 1.4142135623730951 (raiz quadrada de 2)
+//let novoValor = Math.SQRT1_2; // sqrt(1/2) = 0.7071067811865476 (raiz quadrada de 1/2)
+//let novoValor = Math.random(); // Gera um número aleatório entre 0 e 1
+//let novoValor = Math.round(3.54); // Arredonda o número para cima   3.5 -> 4 
+//let novoValor = Math.floor(3.75); // Arredonda o número para baixo
+//let novoValor = Math.ceil(3.75); // Arredonda o número para cima
+//let novoValor = Math.abs(-3.75); // Valor absoluto
+//let novoValor = Math.pow(3, 2); // 3 elevado a 2
+//let novoValor = Math.sqrt(9); // Raiz quadrada
+//let novoValor = Math.min(3, 5, 7, 9); // Menor valor
+//let novoValor = Math.max(3, 5, 7, 9); // Maior valor
+//let novoValor = Math.floor(Math.random() * (max - min + 1)) + min; // Gera um número aleatório entre min e max
+//let novoValor = Math.round(Math.random() * (max - min + 1)) + min; // Gera um número aleatório entre min e max
+let novoValor = Math.tan(45); // Tangente
+//let novoValor = Math.sin(45); // Seno
+//let novoValor = Math.cos(45); // Cosseno
+//let novoValor = Math.log(100); // Logaritmo natural
+//let novoValor = Math.exp(2); // Exponencial
+
+console.log(novoValor); // Imprime o número aleatório gerado
